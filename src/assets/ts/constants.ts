d47e47a532a7eae563263fe81818d801282e662f
azzaz123 <76672419+azzaz123@users.noreply.github.com>
2022-04-18 01:18:19 +0200

    Update constants.ts

--- src/assets/ts/constants.ts
+++ src/assets/ts/constants.ts
@@ -3,12 +3,12 @@ import { Chart } from "../../types/chart";
 /**
  * Base API URL to use for requests.
  */
-export const apiURL = "https://api.zws.im";
+export const apiURL = "https://api.serene-vacherin-4e8413.netlify.app/";
 /**
  * Hostnames of ZWS instances.
  * Used in addition to server-side validation to prevent shortening an already short URL.
  */
-export const hostnames = ["zws.im"];
+export const hostnames = ["serene-vacherin-4e8413.netlify.app"];
 /**
  * HTML elements used across the project.
  */
